0ced0f115be2c9a9e2c19c43138fe61a97875e40
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-02-08 10:37:40 +0100

    Change download script to remove the need to carry on versions.txt

--- download.sh
+++ download.sh
@@ -6,45 +6,86 @@ then
     exit 1
 fi
 
+if ! command -v jq $> /dev/null
+then
+    echo "jq could not be found, please install jq - commandline JSON processor"
+    exit 1
+fi
+
 # Generates a list of versions
-# $1 repository name: orgName/repositoryName
-# $2 isPrerelease: true/false
-# $3 outputFile
+# $1 repositoryName: orgName/repositoryName
+# $2 packageName: app package name
+# $3 isPrerelease: true/false
 load_app_versions () {
-    gh release list -R $1 --limit 200 --json=tagName,isPrerelease --jq ".[] | select(.isPrerelease == $2) | .tagName" --exclude-drafts > $3
+    local repositoryName="$1"
+    local packageName="$2"
+    local isPrerelease="$3"
+
+    local filename=tmp/${packageName}
+
+    gh release list \
+      -R "$repositoryName" \
+      --limit 200 \
+      --json=tagName,isPrerelease,publishedAt \
+      --jq "[.[] | select(.isPrerelease==$isPrerelease)]" \
+      --exclude-drafts \
+      > "${filename}".json
 }
 
 # Download app from provided versions list
-# $1 repository name: orgName/repositoryName
-# $2 number of apps to download
-# $3 inputFile: filename
+# $1 repositoryName: orgName/repositoryName
+# $2 packageName: app package name
+# $3 maxDownloads: maximum number of downloads
+
 # $4 outputFileName: will download as ${outputFileName}-versionName.apk
 load_apps () {
-    counter=1
-    while [ $counter -le $2 ]
-    do
-        version=$(sed "${counter}q;d" $3)
-        file=fdroid/repo/${4}-${version}.apk
-        gh release download -R $1 $version -O $file
-        ((counter++))
+    local repositoryName="$1"
+    local packageName="$2"
+    local maxDownloads="$3"
+
+    local targetFolder="fdroid/repo"
+    local jsonFile="tmp/${packageName}.json"
+    local tags=$(jq -r '.[].tagName' "$jsonFile")
+
+    local count=0
+
+    for tag in $tags; do
+      if (( count < maxDownloads )); then
+        local fileName="${targetFolder}/${packageName}-${tag}.apk"
+        local publishedAt=$(jq -r --arg TAGNAME "$tag" '.[] | select(.tagName==$TAGNAME) | .publishedAt' "$jsonFile")
+        local fileDate=$(echo $publishedAt | sed 's/-//g; s/://g; s/T//; s/Z$//; s/[0-9]\{2\}$//')
+        gh release download -R $repositoryName $tag -O $fileName --clobber
+        touch -t $fileDate $fileName
+        ((count++))
+      else
+        break
+      fi
     done
 }
 
 # Download metadata from given github repository and folder
-# $1 repository name: orgName/repositoryName
+# $1 repositoryName: orgName/repositoryName
 # $2 folderName: app folder to copy metadata from
-# $3 packageName: used for fdroid app
+# $3 packageName: app package name
 update_metadata () {
-    gh repo clone $1 tmp/$1
-    rm -r fdroid/metadata/$3 || true
-    cp -r tmp/$1/$2/fastlane/metadata/android/ fdroid/metadata/$3
+    local repositoryName="$1"
+    local folderName="$2"
+    local packageName="$3"
+
+    local repoFolder="tmp/${repositoryName}"
+    local sourceFolder="${repoFolder}/${folderName}/fastlane/metadata/android/"
+    local targetFolder="fdroid/metadata/$packageName"
+
+    gh repo clone "$repositoryName" "$repoFolder"
+    rm -r "${targetFolder}" || true
+    cp -r "$sourceFolder" "${targetFolder}"
 }
 
 # cleanup any previous downloads
+rm -rf tmp
+
 mkdir -p tmp
 
-load_app_versions thunderbird/thunderbird-android true tmp/k9-beta-versions.txt
-
-load_apps thunderbird/thunderbird-android 20 tmp/k9-beta-versions.txt k9-beta
-
+load_app_versions thunderbird/thunderbird-android com.fsck.k9 true
+load_apps thunderbird/thunderbird-android com.fsck.k9 4
 update_metadata thunderbird/thunderbird-android app-k9mail com.fsck.k9
